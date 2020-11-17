eaaa6b31bbf60f9352dc232ef91f67c5a9fc8f50
amrita0110 <dasamrita1020@gmail.com>
2020-11-17 13:00:47 +0530

    My very first commit

---
+++ Anonymous.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+class Anonymous { 
+    public static void main(String[] args) 
+    { 
+          // anonymous array 
+          sum(new int[]{ 1, 2, 3 }); 
+    } 
+    public static void sum(int[] a) 
+    { 
+        int total = 0; 
+  
+        // using for-each loop 
+        for (int i : a)  
+            total = total + i; 
+          
+        System.out.println("The sum is:" + total); 
+    } 
+} 
---
+++ applet.java
@@ -0,0 +1,12 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class Myapplet extends Applet
+{
+    public void paint(Graphics g)
+    {
+    g.drawString("welcom",150,150);
+    }
+}
+
+
+ 
